cb5bed242e7ff1bc5cf822d00043e33a2d795f5b
Viktoria Rosovska <torimeister@gmail.com>
2023-10-25 17:55:20 +0300

    not found component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from '../src/redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import GlobalStyle from 'styles/GlobalStyled';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="drinkmaster-teamproject">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
+          <GlobalStyle />
           <App />
         </PersistGate>
       </Provider>
