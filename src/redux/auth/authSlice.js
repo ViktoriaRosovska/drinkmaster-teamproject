15f30ecd838b178efd3cb02bb81abd992dea12c0
Viktoria Rosovska <torimeister@gmail.com>
2023-10-25 14:12:06 +0300

    update store

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const authSlice = createSlice({
-  //   name: 'auth',
-  //   initialState: {
-  //     user: { name: '', email: '', avatarURL: '', id: '' },
-  //     token: null,
-  //     isLoggedIn: false,
-  //     isRefreshing: false,
-  //     error: null,
-  //   },
-  //     extraReducers: builder => {
-  //         builder....
-  //     }
+  name: 'auth',
+  initialState: {
+    user: { name: '', email: '', avatarURL: '', id: '' },
+    token: null,
+    //     isLoggedIn: false,
+    //     isRefreshing: false,
+    //     error: null,
+    //   },
+    //     extraReducers: builder => {
+    //         builder....
+  },
 });
 
 export const authReducer = authSlice.reducer;
