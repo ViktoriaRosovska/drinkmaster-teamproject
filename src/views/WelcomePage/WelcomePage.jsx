201f1acb04bd680a79ac6f0c28216e0f08c0ee94
Viktoria Rosovska <torimeister@gmail.com>
2023-10-27 01:21:26 +0300

    fix welcome

--- src/views/WelcomePage/WelcomePage.jsx
+++ src/views/WelcomePage/WelcomePage.jsx
@@ -4,13 +4,3 @@ import AuthNav from 'components/AuthNav/AuthNav';
 export default function WelcomePage() {
   return <AuthNav />;
 }
-
-// export default function WelcomePage() {
-//   return (
-//     <>
-//       <h1>Welcome page</h1>
-//       <a href="/welcome/signin">signin</a>
-//       <a href="/welcome/signup">signup</a>
-//     </>
-//   );
-// }
