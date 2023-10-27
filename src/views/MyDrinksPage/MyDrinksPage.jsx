505859e9d013df30290e6fae51c9306df0d529b7
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-10-27 19:48:19 +0300

    drinks
    Вернула свою страницу с фейворит и компонент нот фаунд.

--- src/views/MyDrinksPage/MyDrinksPage.jsx
+++ src/views/MyDrinksPage/MyDrinksPage.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import DrinkList from 'components/DrinkList/DrinkList';
+import PageTitle from 'components/PageTitle/PageTitle';
+import { MainContainer } from 'styles/App.styled';
+
 export const MyDrinksPage = () => {
-  return <h2>welcome to MyDrinksPage</h2>;
+  return (
+    <MainContainer>
+      <PageTitle title="My drinks" />
+      <DrinkList/> 
+    </MainContainer>
+  );
 };
