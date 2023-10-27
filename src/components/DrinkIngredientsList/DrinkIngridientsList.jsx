04f20ea44eaf09c80b0de5f9445ee0736ee7d57a
OksanaPonom <ksyushono4ek@gmail.com>
2023-10-27 22:27:15 +0300

    add  marking on DrinkIdPage

--- src/components/DrinkIngredientsList/DrinkIngridientsList.jsx
+++ src/components/DrinkIngredientsList/DrinkIngridientsList.jsx
@@ -1,4 +1,25 @@
-const DrinkIngridientList = () => {
-  return <div>DrinkIngridientList</div>;
+const DrinkIngridientList = ({ ingredients }) => {
+  return (
+    <>
+      <p>Ingredients</p>
+      <ul>
+        {ingredients.map(({ title, measure, ingredientThumb }) => (
+          <div key={title}>
+            <li>
+              {ingredientThumb ? (
+                <img src={ingredientThumb} alt={title} />
+              ) : (
+                <div>default image</div>
+              )}
+              <div>
+                <p>{title}</p>
+                <p>{measure}</p>
+              </div>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default DrinkIngridientList;
