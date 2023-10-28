30ef9941cd2acedd700084e55ece2c4a51c25fe2
AnnaFutryk <anna.futryk@gmail.com>
2023-10-28 15:07:12 +0300

    add builder to auth redux

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://drink-master-app.onrender.com';
+axios.defaults.baseURL = 'https://drink-master-app.onrender.com/api/'; //??? треба узгодити з бекендом
 
 const token = {
   set(token) {
