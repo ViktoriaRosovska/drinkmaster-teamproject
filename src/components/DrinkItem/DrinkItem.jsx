261027466eab8f9de0eb734ee20c1acb55a61249
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-10-28 09:43:19 +0300

    link

--- src/components/DrinkItem/DrinkItem.jsx
+++ src/components/DrinkItem/DrinkItem.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Card, CardImage, DrinkName, IsAlco, Description, ButtonsContainer, SVG, DeleteBtn } from './DrinkItem.styled';
+import {
+  Card,
+  CardImage,
+  DrinkName,
+  IsAlco,
+  Description,
+  ButtonsContainer,
+  SVG,
+  DeleteBtn,
+} from './DrinkItem.styled';
 import trash from '../../assets/images/deleteSvg/trash.svg';
 import SeeMoreBtn from 'components/LinkBtn/SeeMoreBtn';
 
 const DrinkItem = ({ drinkData }) => {
-    const { drink, drinkThumb, alcoholic, description, _id } = drinkData;
+  const { drink, drinkThumb, alcoholic, description, _id } = drinkData;
 
-    return (
-        <Card>
-            <CardImage src={drinkThumb} alt={drink} />
-            <DrinkName>{drink}</DrinkName>
-            <IsAlco>{alcoholic}</IsAlco>
-            <Description>{description}</Description>
-            <ButtonsContainer>
-                <SeeMoreBtn navLink={`/drink/${_id}`} />
-            <DeleteBtn type="button">
+  return (
+    <Card>
+      <CardImage src={drinkThumb} alt={drink} />
+      <DrinkName>{drink}</DrinkName>
+      <IsAlco>{alcoholic}</IsAlco>
+      <Description>{description}</Description>
+      <ButtonsContainer>
+        <SeeMoreBtn navLink={`/drink/${_id}`} />
+        <DeleteBtn type="button">
           <SVG src={trash} />
-                </DeleteBtn>
-                </ButtonsContainer>
-       </Card>
-   )
-}
+        </DeleteBtn>
+      </ButtonsContainer>
+    </Card>
+  );
+};
 
 export default DrinkItem;
