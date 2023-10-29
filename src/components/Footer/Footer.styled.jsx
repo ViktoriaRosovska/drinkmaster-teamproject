668d63979f7e608bb05acced475589e20a8be319
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-10-29 19:45:16 +0200

    add height, delite changing color to hover folowUs

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -6,31 +6,64 @@ display: flex:
 margin-top: 40px;
 margin-bottom: 18px;
 min-width: 320px;
-height: 100%;
+
 background: radial-gradient(
       circle,
-      var(--light-green-forty-color) 40%,
+      var(--light-green-forty-color) 10%,
       rgba(255, 255, 255, 0) 70%
     ),
   
   background-size: 774px 762px;
   background-repeat: no-repeat;
-  background-position-x: -372px;
-  background-position-y: 3219px;
+  background-position-x: -260px;
+  background-position-y: 330px;
 
  @media screen and (min-width: 768px) {
     margin-top: 80px;
     margin-bottom: 24px;
-   
-  background-position-x: -63px;
-  background-position-y: 3839px;
+    
+   background: radial-gradient(
+        circle,
+        rgba(188, 230, 210, 0.4) 10%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      radial-gradient(
+        circle,
+        rgba(64, 112, 205, 0.5) 0%,
+        rgba(255, 255, 255, 0) 65%
+      ),
+      
+    background-size:
+      1100px 1100px,
+      900px 900px;
+
+    background-repeat: no-repeat;
+
+    background-position-x: calc(40vw - 550px), calc(60vw - 300px);
+    background-position-y: 260px, 350px;
   }
 
-@media screen and (min-width: 768px) {
-background-position-x: 193px;
-  background-position-y: 3176px;
-}
+@media screen and (min-width: 1440px) {
+  background: radial-gradient(
+        circle,
+        rgba(188, 230, 210, 0.4) 30%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      radial-gradient(
+        circle,
+        rgba(64, 112, 205, 0.5) 0%,
+        rgba(255, 255, 255, 0) 65%
+      ),
+     
+    background-size:
+      1250px 1250px,
+      900px 900px;
 
+    background-repeat: no-repeat, no-repeat;
+
+    background-position-x: calc(50vw - 650px), calc(60vw - 400px);
+    background-position-y: 260px, 270px;
+}
 `;
 
 export const FooterLogoAndSocial = styled.div`
@@ -48,19 +81,22 @@ export const Container = styled.div`
   font-size: 14px;
   font-weight: 500;
   min-width: 320px;
-  min-height: 100vh;
+  height: 542px;
+  
   padding-left: 20px;
   padding-right: 20px;
   padding-top: 80px;
   
   @media screen and (min-width: 768px) {
     width: 704px;
+    height: 482px;
     padding-left: 32px;
     padding-right: 32px;
   }
 
   @media screen and (min-width: 1440px) {
     width: 1240px;
+    height: 428px;
     padding-left: 100px;
     padding-right: 100px;
   }
