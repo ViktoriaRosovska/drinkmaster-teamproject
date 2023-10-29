ade751d8908a936fc379aee565d88430ac146b4b
Viktoria Rosovska <torimeister@gmail.com>
2023-10-29 13:33:07 +0200

    change routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 import { HomePage } from 'views/HomePage/HomePage';
 import { DrinksPage } from 'views/DrinksPage/DrinksPage';
@@ -17,33 +17,70 @@ import DrinkIdPage from 'views/DrinkIdPage/DrinkIdPage';
 import { useEffect } from 'react';
 
 const isLoggedIn = true;
-
 export const App = () => {
-  useEffect(() => {}, []);
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/home');
+    }
+  }, [location.pathname, navigate]);
   return (
     <>
       <Routes>
         <Route
+          path="/welcome"
           element={
-            <PrivateRoute redirectTo="/welcome" isLoggedIn={isLoggedIn} />
+            <PublicRoute
+              // restricted={false}
+              redirectTo="/home"
+              isLoggedIn={isLoggedIn}
+              component={<WelcomePage />}
+            />
+          }
+        />
+
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute
+              // restricted={false}
+              redirectTo="/home"
+              isLoggedIn={isLoggedIn}
+              component={<SigninForm />}
+            />
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute
+              // restricted={false}
+              redirectTo="/home"
+              isLoggedIn={isLoggedIn}
+              component={<SignupForm />}
+            />
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <PrivateRoute
+              redirectTo="/welcome"
+              isLoggedIn={isLoggedIn}
+              component={<SharedLayout />}
+            />
           }
         >
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/drinks" element={<DrinksPage />} />
-            <Route path="/drinks/Id" element={<DrinkIdPage />} />
-            <Route path="/add" element={<AddDrinkPage />} />
-            <Route path="/my" element={<MyDrinksPage />} />
-            <Route path="/favorites" element={<FavoriteDrinksPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Route>
-        <Route
-          element={<PublicRoute redirectTo="/home" isLoggedIn={isLoggedIn} />}
-        >
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/signin" element={<SigninForm />} />
-          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/drinks" element={<DrinksPage />} />
+          <Route path="/drinks/drinkId" element={<DrinkIdPage />} />
+          <Route path="/add" element={<AddDrinkPage />} />
+          <Route path="/my" element={<MyDrinksPage />} />
+          <Route path="/favorites" element={<FavoriteDrinksPage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </>
