471d1ce9fc4c0ba81874aa3d05f346188865f0f0
Viktoria Rosovska <torimeister@gmail.com>
2023-10-29 22:55:44 +0200

    update

--- src/components/LinkBtn/WhiteLinkBtn/WhiteLinkBtn.js
+++ src/components/LinkBtn/WhiteLinkBtn/WhiteLinkBtn.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import { WhiteButton } from './WhiteLinkBtn.styled';
 
 const WhiteLinkBtn = ({ navLink, title }) => {
-  return (
-    <Link to={navLink}>
-      <WhiteButton type="button">{title}</WhiteButton>
-    </Link>
-  );
+  return <WhiteButton type="button">{title}</WhiteButton>;
 };
 
 export default WhiteLinkBtn;
