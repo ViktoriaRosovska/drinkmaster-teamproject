74adc765da767c9300b5d66a6bd353cd19220cfb
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-10-29 21:17:33 +0100

    style-welcome

--- src/views/WelcomePage/WelcomePage.styled.js
+++ src/views/WelcomePage/WelcomePage.styled.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
-export const Conteiner = styled.div`
-  color: #fafafa;
-  max-width: 485px;
+export const Cont = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Container = styled.div`
+  color: var(--start-page-txt-color);
+  max-width: 485px;
+  margin-left: 100px;
+  position: relative;
 `;
 
 export const H2 = styled.h2`
@@ -23,12 +28,59 @@ export const ButtonDiv = styled.div`
 
 export const DefButton = styled.button`
   background-color: transparent;
-  color: #f3f3f3;
+  color: var(--white-color);
   padding: 18px 44px 18px 44px;
   border-radius: 42px;
   border: rgba(243, 243, 243, 0.2) solid 1px;
   &:hover {
-    background-color: #f3f3f3;
-    color: #161f37;
+    background-color: var(--white-color);
+    color: var(--dark-blue-color);
   }
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  max-width: 100%;
+  height: 100%;
+`;
+
+export const ImgWithGradient = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  background: linear-gradient(to right, transparent 0%, rgba(0, 0, 0, 1) 100%);
+`;
+
+export const Ellipse223 = styled.div`
+  position: absolute;
+  width: 387px;
+  height: 381px;
+  left: 171px;
+  top: -132px;
+  background: rgba(188, 230, 210, 0.4);
+  filter: blur(104.854px);
+`;
+
+export const Ellipse222 = styled.div`
+  position: absolute;
+  width: 784px;
+  height: 849px;
+  left: -494px;
+  top: -231px;
+  background: rgba(64, 112, 205, 0.5);
+  filter: blur(104.854px);
+`;
+
+export const Ellipse224 = styled.div`
+  position: absolute;
+  width: 549px;
+  height: 543px;
+  left: 1781px;
+  top: 858px;
+  background: rgba(64, 112, 205, 0.5);
+  filter: blur(104.854px);
+  z-index: 2;
 `;
