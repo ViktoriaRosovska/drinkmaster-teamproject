1a9dae9987b840fbf1ce19a833479abac38aeb1f
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-10-31 01:19:19 +0200

    Paginator with mui

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,57 +1,69 @@
-import React, { useState } from 'react';
-// import { Pagination } from '@mui/material';
-import prev from '../../assets/images/paginator/prev.svg';
-import next from '../../assets/images/paginator/next.svg';
-import { ReactSVG } from 'react-svg';
-import { Button, PagContainer, NumberButton, NumberContainer } from './Paginator.styled';
+import React from 'react';
 
-const Paginator = ({totalDrinks, drinksPerPage}) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const totalPages = Math.ceil(totalDrinks / drinksPerPage);
+import Stack from '@mui/material/Stack';
+import { StyledPagination, stackStyles } from './Paginator.styled';
+// import { createTheme, ThemeProvider } from '@mui/styles';
 
-    const handlePrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+// import prev from '../../assets/images/paginator/prev.svg';
+// import next from '../../assets/images/paginator/next.svg';
+// import { ReactSVG } from 'react-svg';
+// import { Button, PagContainer, NumberButton, NumberContainer } from './Paginator.styled';
 
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
+const Paginator = () => {
+  return (
+    <Stack spacing={2} sx={stackStyles}>
+      <StyledPagination count={15} />
+    </Stack>
+  );
+};
 
-    if (totalPages <= 1) {
-        return null;
-    };
 
-    const visiblePageCount = 5;
-    const halfVisiblePageCount = Math.floor(visiblePageCount / 2);
 
-    const firstVisiblePage = Math.max(1, currentPage - halfVisiblePageCount);
-    const lastVisiblePage = Math.min(totalPages, firstVisiblePage + visiblePageCount - 1);
+// const Paginator = ({totalDrinks, drinksPerPage}) => {
+//     const [currentPage, setCurrentPage] = useState(1);
+//     const totalPages = Math.ceil(totalDrinks / drinksPerPage);
 
-    return (
-        <PagContainer>
-            <Button type='button' onClick={handlePrevPage} disabled={currentPage === 1}>
-                <ReactSVG src={prev}/>
-            </Button>
-            <NumberContainer>
-            {Array.from({ length: lastVisiblePage - firstVisiblePage + 1 }, (_, index) => (
-                <NumberButton
-                    key={index}
-                    onClick={() => setCurrentPage(firstVisiblePage + index)}
-                    active={firstVisiblePage + index === currentPage}
-                >{firstVisiblePage + index }</NumberButton>
-            ))}
-                </NumberContainer>
-            <Button type='button' onClick={handleNextPage} disabled={currentPage === totalPages}>
-                <ReactSVG src={next}/>
-            </Button>
-        </PagContainer>
-    )
-}
+//     const handlePrevPage = () => {
+//         if (currentPage > 1) {
+//             setCurrentPage(currentPage - 1);
+//         }
+//     };
 
+//     const handleNextPage = () => {
+//         if (currentPage < totalPages) {
+//             setCurrentPage(currentPage + 1);
+//         }
+//     };
+
+//     if (totalPages <= 1) {
+//         return null;
+//     };
+
+//     const visiblePageCount = 5;
+//     const halfVisiblePageCount = Math.floor(visiblePageCount / 2);
+
+//     const firstVisiblePage = Math.max(1, currentPage - halfVisiblePageCount);
+//     const lastVisiblePage = Math.min(totalPages, firstVisiblePage + visiblePageCount - 1);
+
+//     return (
+//         <PagContainer>
+//             <Button type='button' onClick={handlePrevPage} disabled={currentPage === 1}>
+//                 <ReactSVG src={prev}/>
+//             </Button>
+//             <NumberContainer>
+//             {Array.from({ length: lastVisiblePage - firstVisiblePage + 1 }, (_, index) => (
+//                 <NumberButton
+//                     key={index}
+//                     onClick={() => setCurrentPage(firstVisiblePage + index)}
+//                     active={firstVisiblePage + index === currentPage}
+//                 >{firstVisiblePage + index }</NumberButton>
+//             ))}
+//                 </NumberContainer>
+//             <Button type='button' onClick={handleNextPage} disabled={currentPage === totalPages}>
+//                 <ReactSVG src={next}/>
+//             </Button>
+//         </PagContainer>
+//     )
+// }
 
 export default Paginator;
-
--- src/components/Paginator/Paginator.styled.jsx
+++ src/components/Paginator/Paginator.styled.jsx
@@ -1,48 +1,73 @@
-import styled from 'styled-components';
+import { styled } from '@mui/material/styles';
+import Pagination from '@mui/material/Pagination';
 
-export const PagContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  /* gap: 40px; */
-  padding-top: 14px;
-  padding-bottom: 14px;
-  margin-top: 40px;
-  width: 295px;
+export const StyledPagination = styled(Pagination)({
+    '& .MuiPagination-ul': {
+        justifyContent: 'center',
+    }, 
+    '& li .Mui-selected': {
+        color: '#f3f3f3',
+        backgroundColor: 'rgba(64, 112, 205, 0.5)',
+        ':hover': {
+            backgroundColor: 'rgba(64, 112, 205, 0.5)',
+        }
+    },
+    '& li .MuiPaginationItem-root': {
+        color: '#f3f3f3',
+    },
+    '& .MuiPaginationItem-ellipsis': {
+        color: '#f3f3f3',
+    }
+})
+export const stackStyles = {
+    marginTop: '54px',
+    justifyContent: 'center',
 
-  /* @media screen and (min-width: 768px) {
-    gap: 40px;
-  } */
-`;
+ }
 
-export const NumberContainer = styled.div`
-  display: flex;
-  gap: 18px;
-  align-items: center;
-  justify-content: center;
-`;
 
-export const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: transparent;
-`;
 
-export const NumberButton = styled.button`
-  background-color: transparent;
-  color: var(--white-color);
-  font-weight: 500;
-  font-size: 12px;
-  width: 27px;
-  height: 27px;
-  border-radius: 50%;
+// import styled from 'styled-components';
 
-  ${({active}) => active? `
-     background-color: var(--blue-fifty-color);
-  ` : ' '}
+// export const PagContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding-top: 14px;
+//   padding-bottom: 14px;
+//   margin-top: 40px;
+//   width: 295px;
 
-  &:hover {
-    background-color: var(--blue-fifty-color);
-  }
-`;
+// `;
+
+// export const NumberContainer = styled.div`
+//   display: flex;
+//   gap: 18px;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+// export const Button = styled.button`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   background-color: transparent;
+// `;
+
+// export const NumberButton = styled.button`
+//   background-color: transparent;
+//   color: var(--white-color);
+//   font-weight: 500;
+//   font-size: 12px;
+//   width: 27px;
+//   height: 27px;
+//   border-radius: 50%;
+
+//   ${({active}) => active? `
+//      background-color: var(--blue-fifty-color);
+//   ` : ' '}
+
+//   &:hover {
+//     background-color: var(--blue-fifty-color);
+//   }
+// `;
