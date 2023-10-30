c2afe2bcfb93492476483cd154b2e6900fbc70bd
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-10-30 01:07:22 +0100

    feat/header

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -2,40 +2,28 @@ import React from 'react';
 import {
   H2,
   P,
-  Cont,
   Container,
   DefButton,
   ButtonDiv,
-  Img,
-  Ellipse223,
-  Ellipse222,
-  Ellipse224,
-  ImgWithGradient,
 } from 'views/WelcomePage/WelcomePage.styled';
-import myImage from 'assets/images/welcomePage/bg-welcome-desc@2x.jpg';
+import { WelcomeWrapper } from 'styles/App.styled';
 
 const AuthNav = () => {
   return (
-    <Cont>
-      <Ellipse223></Ellipse223>
-      <Ellipse222></Ellipse222>
-      <Ellipse224></Ellipse224>
-      <Container>
-        <H2>Welcome to the app!</H2>
-        <P>
-          This app offers more than just a collection of recipes - it is
-          designed to be your very own digital cookbook. You can easily save and
-          retrieve your own recipes at any time.
-        </P>
-        <ButtonDiv>
-          <DefButton to="/signup">Sign Up</DefButton>
-          <DefButton to="/signin">Sign In</DefButton>
-        </ButtonDiv>
-      </Container>
-      <ImgWithGradient>
-        <Img src={myImage} alt="Cocktail" />
-      </ImgWithGradient>
-    </Cont>
+    <WelcomeWrapper>
+        <Container>
+          <H2>Welcome to the app!</H2>
+          <P>
+            This app offers more than just a collection of recipes - it is
+            designed to be your very own digital cookbook. You can easily save
+            and retrieve your own recipes at any time.
+          </P>
+          <ButtonDiv>
+            <DefButton to="/signup">Sign Up</DefButton>
+            <DefButton to="/signin">Sign In</DefButton>
+          </ButtonDiv>
+        </Container>
+    </WelcomeWrapper>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,33 @@
-// Header component
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from 'components/Logo/Logo';
+import { Container, Ul } from 'components/Header/Header.styled';
+
+const Header = () => {
+  return (
+    <Container>
+      <Ul>
+        <li>
+          <Logo />
+        </li>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/drinks">Drinks</NavLink>
+        </li>
+        <li>
+          <NavLink to="/add">Add drink</NavLink>
+        </li>
+        <li>
+          <NavLink to="/my">My drinks</NavLink>
+        </li>
+        <li>
+          <NavLink to="/favorites">Favorites</NavLink>
+        </li>
+      </Ul>
+    </Container>
+  );
+};
+
+export default Header;
