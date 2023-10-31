d5ff2a17c2d243016a12a783f39767b5e9d59a22
Viktoria Rosovska <torimeister@gmail.com>
2023-10-31 21:14:57 +0200

    add fonts

--- src/styles/GlobalStyled.jsx
+++ src/styles/GlobalStyled.jsx
@@ -13,6 +13,7 @@ h5,
 h6,
 p {
   margin: 0;
+  padding: 0;
 }
 
 ul,
@@ -34,6 +35,38 @@ button {
 
 img {
   display: block;
+}
+@font-face {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(../assets/fonts/Manrope-Regular.woff2) format('woff2'),
+    url(../assets/fonts/Manrope-Regular.woff) format('woff'),
+    url(../assets/fonts/Manrope-Regular.ttf) format('ttf'),
+    url(../assets//fonts/Manrope-Regular.eot) format('eot');
+}
+
+@font-face {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url(../assets/fonts/Manrope-Medium.woff2) format('woff2'),
+    url(../assets/fonts/Manrope-Medium.woff) format('woff'),
+    url(../assets/fonts/Manrope-Medium.ttf) format('ttf'),
+    url(../assets/fonts/Manrope-Medium.eot) format('eot');
+}
+
+@font-face {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url(../assets/fonts/Manrope-Semibold.woff2) format('woff2'),
+    url(../assets/fonts/Manrope-Semibold.woff) format('woff'),
+    url(../assets/fonts/Manrope-Semibold.ttf) format('ttf'),
+    url(../assets/fonts/Manrope-Semibold.eot) format('eot');
 }
 
 
