33d72f75deee91a2451ad43b4171ce5ee4d63381
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-10-31 03:53:51 +0100

    feat/welcome/header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import Logo from 'components/Logo/Logo';
-import { Container, Ul } from 'components/Header/Header.styled';
+import Nav from 'components/Nav/Nav';
+import {
+  HeaderCont,
+  Container,
+  NavCont,
+} from 'components/Header/Header.styled';
 
 const Header = () => {
   return (
-    <Container>
-      <Ul>
-        <li>
-          <Logo />
-        </li>
-        <li>
-          <NavLink to="/home">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/drinks">Drinks</NavLink>
-        </li>
-        <li>
-          <NavLink to="/add">Add drink</NavLink>
-        </li>
-        <li>
-          <NavLink to="/my">My drinks</NavLink>
-        </li>
-        <li>
-          <NavLink to="/favorites">Favorites</NavLink>
-        </li>
-      </Ul>
-    </Container>
+    <HeaderCont>
+      <Container>
+        <Logo />
+        <NavCont>
+          <Nav />
+        </NavCont>
+      </Container>
+    </HeaderCont>
   );
 };
 
--- src/views/WelcomePage/WelcomePage.styled.js
+++ src/views/WelcomePage/WelcomePage.styled.js
@@ -1,23 +1,55 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   color: var(--start-page-txt-color);
-  max-width: 485px;
-  margin-left: 100px;
   position: relative;
+  max-width: 335px;
+  margin-left: 20px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 470px;
+    margin-left: 64px;
+    align-items: start;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 485px;
+    margin-left: 100px;
+    align-items: start;
+  }
 `;
 
 export const H2 = styled.h2`
   margin-bottom: 14px;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 32px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 40px;
+    line-height: 44px;
+  }
 `;
 
 export const P = styled.p`
   margin-bottom: 40px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 24px;
+  }
 `;
 
 export const ButtonDiv = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 14px;
 `;
 
 export const DefButton = styled.button`
