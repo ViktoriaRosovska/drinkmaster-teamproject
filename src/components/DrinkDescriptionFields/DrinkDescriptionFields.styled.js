a36e26c2dd388e4f40cdb448b610c3d202c2e43c
Viktoria Rosovska <torimeister@gmail.com>
2023-10-31 20:26:32 +0200

    fix

--- src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
+++ src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
@@ -1,5 +1,6 @@
 import { ReactSVG } from 'react-svg';
 import styled from 'styled-components';
+import Select from 'react-select';
 
 const AddPhotoContainer = styled.div`
   width: 335px;
@@ -46,4 +47,217 @@ const AddBtnText = styled.span`
 
   color: var(--white-color);
 `;
-export { AddPhotoContainer, AddBtn, ReactSVGIcon, AddBtnText };
+const SelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 31px;
+  margin-bottom: 81px;
+`;
+
+const AddFormSelect = styled(Select)`
+  padding: 0;
+  width: 100%;
+  height: 34px;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  outline: none;
+  & .css-1dimb5e-singleValue {
+    line-height: 1;
+    text-align: left;
+    height: 34px;
+
+    color: var(--white-color);
+
+    padding: 0;
+  }
+  & .css-1fdsijx-ValueContainer {
+    padding: 0;
+    & ::selection {
+      background-color: transparent;
+    }
+  }
+  // Випадаюче меню списка
+  & #react-select-3-listbox {
+    font-size: 12px;
+    line-height: 16px;
+
+    background-color: var(--dark-blue-color);
+    color: rgba(243, 243, 243, 0.4);
+    border-radius: 12px;
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+
+    /* padding: 10px; */
+    /* overflow-y: scroll; */
+    overflow: hidden;
+    position: absolute;
+    width: 131px;
+    height: 256px;
+    right: 0;
+    ::-webkit-scrollbar {
+      /* width: 5px; */
+      width: 0;
+    }
+  }
+  & .css-13cymwt-control {
+    border: none;
+    border-bottom: 1px solid var(--white-color);
+  }
+  & .css-1dimb5e-singleValue {
+    margin-right: 28px;
+  }
+  & .css-qbdosj-Input {
+    line-height: normal;
+    letter-spacing: -0.28px;
+
+    color: var(--white-color);
+
+    height: 34px;
+    padding: 0;
+    margin-right: 28px;
+    margin: 0 0 0 60px;
+  }
+
+  & .css-166bipr-Input {
+    color: var(--white-color);
+    margin-left: 60px;
+  }
+
+  & .css-13cymwt-control {
+    line-height: 14px;
+    letter-spacing: 0em;
+
+    background-color: transparent;
+    color: var(--white-color);
+
+    height: 34px;
+  }
+  & .css-1xc3v61-indicatorContainer {
+    display: none;
+    outline: none;
+  }
+  & .css-1hb7zxy-IndicatorsContainer {
+    display: none;
+  }
+  & .react-select-3-placeholder {
+    display: block;
+  }
+  & .css-1dimb5e-singleValue {
+    text-align: right;
+  }
+  & .css-1u9des2-indicatorSeparator {
+    display: none;
+  }
+  & #react-select-3-input {
+    text-align: right;
+    margin-left: 50px;
+    height: 34px;
+  }
+  & #react-select-5-listbox {
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    width: 131px;
+    height: 258px;
+    padding: 0;
+    background-color: var(--dark-blue-color);
+    color: rgba(243, 243, 243, 0.4);
+    font-size: 12px;
+    line-height: 16px;
+    padding: 10px;
+
+    //стилізувати скрол
+
+    /* overflow-y: scroll; */
+    overflow: hidden;
+    position: absolute;
+    right: 0;
+    ::-webkit-scrollbar {
+      /* width: 5px; */
+      width: 0;
+    }
+    & :hover {
+      background-color: none;
+    }
+  }
+`;
+
+const AddFormRadioGroup = styled.div`
+  display: flex;
+  gap: 14px;
+  flex-direction: row;
+`;
+
+const AddFormInput = styled.input`
+  line-height: 14px;
+  letter-spacing: 0em;
+
+  border: none;
+  border-bottom: 1px solid var(--white-fifty-color);
+
+  color: var(--white-color);
+  background-color: transparent;
+  position: relative;
+  width: 335px;
+  height: 34px;
+  outline: none;
+  &:hover {
+    border-bottom: 1px solid var(--white-color);
+  }
+  &::placeholder {
+    font-family: 'Manrope';
+    line-height: normal;
+    letter-spacing: -0.28px;
+    position: absolute;
+    top: 0;
+    color: var(--white-color);
+
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: -0.02em;
+    text-align: left;
+
+    /* color: red; */
+  }
+`;
+const InputRadio = styled.input`
+  /* display: none; */
+`;
+const ChevronReactSvg = styled(ReactSVG)`
+  width: 20px;
+  height: 20px;
+  /* opacity: 0.5; */
+  & div svg {
+    stroke: var(--white-color);
+  }
+`;
+const AddFormSelectLabel = styled.label`
+  line-height: 19px;
+  letter-spacing: -0.02em;
+
+  color: var(--white-fifty-color);
+
+  display: inline-flex;
+  justify-content: space-between;
+  text-align: left;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 335px;
+`;
+export {
+  AddPhotoContainer,
+  AddBtn,
+  ReactSVGIcon,
+  AddBtnText,
+  SelectContainer,
+  AddFormInput,
+  AddFormRadioGroup,
+  AddFormSelect,
+  InputRadio,
+  ChevronReactSvg,
+  AddFormSelectLabel,
+};
--- src/components/Forms/AddDrinkForm/AddDrinkForm.js
+++ src/components/Forms/AddDrinkForm/AddDrinkForm.js
@@ -1,42 +1,35 @@
-import { useState } from 'react';
 import minuIcon from '../../../assets/images/addDrink/MinusForm.svg';
 import plusIcon from '../../../assets/images/addDrink/PlusForm.svg';
 import closeIcon from '../../../assets/images/addDrink/X.svg';
 import {
-  AddFormInput,
-  AddFormRadioGroup,
   AddFormSelect,
   AddFormSelectContainer,
-  AddFormSelectLabel,
   AddFormTextarea,
   AddIngedientSelect,
   ChevronReactSvg,
   CloseIconReactSvg,
-  InputRadio,
   QuantityBtn,
   QuantityBtnMirror,
   QuantityContainer,
   QuantityIconReactSvg,
   QuantityIngredients,
   QuantityText,
-  SelectContainer,
   SubTitle,
 } from './AddDrinkForm.styled';
 import WhiteLinkBtn from '../../Buttons/WhiteLinkBtn/WhiteLinkBtn';
 
 import ingredients from '../../../helpers/Data/ingredients.json';
-import glasses from '../../../helpers/Data/glasses';
+
 import chevronDown from '../../../assets/images/addDrink/chevron-down.svg';
 import { DrinkDescriptionFields } from '../../DrinkDescriptionFields/DrinkDescriptionFields';
+import { useState } from 'react';
 // import chevronUp from '../../assets/images/addDrink/chevron-up.svg';
 // import { ReactSVG } from 'react-svg';
 
 export default function AddDrinkForm() {
-  const [value, setValue] = useState('Alcogolic');
   const [quantityValue, setQuantityValue] = useState(1);
-  const changeValue = e => {
-    setValue(e.currentTarget.value);
-  };
+  // const changeValue = e => {
+  // setValue(e.currentTarget.value);
 
   const onFormSubmit = e => {
     e.preventDefault();
@@ -58,33 +51,34 @@ export default function AddDrinkForm() {
       setQuantityValue(quantityValue);
     }
   };
+
   return (
-    <form onSubmit={onFormSubmit} style={{ marginBottom: '80px' }}>
-      <DrinkDescriptionFields />
-      <SelectContainer>
-        <AddFormInput
-          type="text"
-          data-limit="50"
-          placeholder="Enter item title"
-        />
-        <AddFormInput type="text" placeholder="Enter about recipe" />
-        <AddFormSelectContainer>
-          <AddFormSelectLabel>
-            Category <ChevronReactSvg src={chevronDown} />
-          </AddFormSelectLabel>
-          <AddFormSelect
-            placeholder=""
-            getOptionLabel={ingredient => ingredient.title}
-            getOptionValue={ingredient => ingredient.title}
-            options={ingredients}
+    <>
+      <form onSubmit={onFormSubmit} style={{ marginBottom: '80px' }}>
+        <DrinkDescriptionFields />
+
+        <>
+          <QuantityContainer>
+            <SubTitle>Ingredients</SubTitle>
+            <QuantityIngredients>
+              <QuantityBtn onClick={onMinusQuantityHandler}>
+                <QuantityIconReactSvg src={minuIcon} />
+              </QuantityBtn>
+              <QuantityText>{quantityValue}</QuantityText>
+              <QuantityBtnMirror onClick={onPlusQuantityHandler}>
+                <QuantityIconReactSvg src={plusIcon} />
+              </QuantityBtnMirror>
+            </QuantityIngredients>
+          </QuantityContainer>
+          <AddIngedientSelect
             styles={{
               control: base => ({
                 base,
                 backgroundColor: 'transparent',
                 boxShadow: 'none',
-                borderBottom: '1px solid var(--white-fifty-color)',
+                border: '1px solid var(--white-fifty-color)',
                 '&:hover': {
-                  borderBottom: '1px solid var(--white-color)',
+                  border: '1px solid var(--white-color)',
                 },
               }),
               option: (provided, state) => ({
@@ -96,167 +90,58 @@ export default function AddDrinkForm() {
                 },
               }),
             }}
-          />
-        </AddFormSelectContainer>
+          >
+            <AddFormSelectContainer>
+              <ChevronReactSvg src={chevronDown} />
 
-        <AddFormSelectContainer>
-          <AddFormSelectLabel>
-            Glass <ChevronReactSvg src={chevronDown} />
-          </AddFormSelectLabel>
-          <AddFormSelect
-            className="react-select"
-            classNamePrefix="react-select"
-            placeholder=""
-            name="glassSelect"
-            getOptionLabel={glass => glass.glass}
-            getOptionValue={glass => glass.glass}
-            options={glasses}
-            styles={{
-              control: base => ({
-                base,
+              <AddFormSelect
+                placeholder="Ingredient"
+                getOptionLabel={ingredient => ingredient.title}
+                getOptionValue={ingredient => ingredient.title}
+                options={ingredients}
+                styles={{
+                  control: base => ({
+                    base,
+                    backgroundColor: 'transparent',
+                    boxShadow: 'none',
+                    border: '1px solid var(--white-fifty-color)',
+                    borderRadius: '200px',
+                    width: '200px',
+                    height: '50px',
+                    '&:hover': {
+                      border: '1px solid var(--white-color)',
+                    },
+                  }),
+                  option: (provided, state) => ({
+                    ...provided,
+
+                    backgroundColor: 'none',
+                    '&:hover': {
+                      color: 'var(--white-fifty-color)',
+                    },
+                  }),
+                }}
+              />
+            </AddFormSelectContainer>
+            <input
+              type="text"
+              style={{
+                width: '101px',
+                height: '50px',
+                borderRadius: '200px',
+                border: '1px solid var(--white-fifty-color)',
                 backgroundColor: 'transparent',
-                boxShadow: 'none',
-                borderBottom: '1px solid var(--white-fifty-color)',
-                '&:hover': {
-                  borderBottom: '1px solid var(--white-color)',
-                },
-              }),
-              option: (provided, state) => ({
-                ...provided,
-
-                backgroundColor: 'none',
-                '&:hover': {
-                  color: 'var(--white-color)',
-                },
-              }),
-            }}
-          />
-        </AddFormSelectContainer>
-        <AddFormRadioGroup>
-          <InputRadio
-            type="radio"
-            id="typeAlco"
-            name="typeDrink"
-            value="Alcoholic"
-            checked={value === 'Alcoholic' ? true : false}
-            onChange={e => changeValue(e)}
-          />
-          <label
-            htmlFor="typeAlco"
-            id="typeAlcoLabel"
-            style={{
-              color:
-                value === 'Alcoholic' ? '#F3F3F3' : 'rgba(243, 243, 243, 0.5)',
-            }}
-          >
-            Alcoholic
-          </label>
-          <InputRadio
-            type="radio"
-            id="typeNonAlco"
-            name="typeDrink"
-            value="Non-alcogolic"
-            checked={value === 'Non-alcogolic' ? true : false}
-            onChange={e => changeValue(e)}
-          />
-          <label
-            htmlFor="typeNonAlco"
-            id="typeNonAlcoLabel"
-            style={{
-              color:
-                value === 'Non-alcogolic'
-                  ? '#F3F3F3'
-                  : 'rgba(243, 243, 243, 0.5)',
-            }}
-          >
-            Non-alcoholic
-          </label>
-        </AddFormRadioGroup>
-      </SelectContainer>
-      <>
-        <QuantityContainer>
-          <SubTitle>Ingredients</SubTitle>
-          <QuantityIngredients>
-            <QuantityBtn onClick={onMinusQuantityHandler}>
-              <QuantityIconReactSvg src={minuIcon} />
-            </QuantityBtn>
-            <QuantityText>{quantityValue}</QuantityText>
-            <QuantityBtnMirror onClick={onPlusQuantityHandler}>
-              <QuantityIconReactSvg src={plusIcon} />
-            </QuantityBtnMirror>
-          </QuantityIngredients>
-        </QuantityContainer>
-        <AddIngedientSelect
-          styles={{
-            control: base => ({
-              base,
-              backgroundColor: 'transparent',
-              boxShadow: 'none',
-              border: '1px solid var(--white-fifty-color)',
-              '&:hover': {
-                border: '1px solid var(--white-color)',
-              },
-            }),
-            option: (provided, state) => ({
-              ...provided,
-
-              backgroundColor: 'none',
-              '&:hover': {
-                color: 'var(--white-color)',
-              },
-            }),
-          }}
-        >
-          <AddFormSelectContainer>
-            <ChevronReactSvg src={chevronDown} />
-
-            <AddFormSelect
-              placeholder="Ingredient"
-              getOptionLabel={ingredient => ingredient.title}
-              getOptionValue={ingredient => ingredient.title}
-              options={ingredients}
-              styles={{
-                control: base => ({
-                  base,
-                  backgroundColor: 'transparent',
-                  boxShadow: 'none',
-                  border: '1px solid var(--white-fifty-color)',
-                  borderRadius: '200px',
-                  width: '200px',
-                  height: '50px',
-                  '&:hover': {
-                    border: '1px solid var(--white-color)',
-                  },
-                }),
-                option: (provided, state) => ({
-                  ...provided,
-
-                  backgroundColor: 'none',
-                  '&:hover': {
-                    color: 'var(--white-fifty-color)',
-                  },
-                }),
               }}
             />
-          </AddFormSelectContainer>
-          <input
-            type="text"
-            style={{
-              width: '101px',
-              height: '50px',
-              borderRadius: '200px',
-              border: '1px solid var(--white-fifty-color)',
-              backgroundColor: 'transparent',
-            }}
-          />
-          <CloseIconReactSvg src={closeIcon} />
-        </AddIngedientSelect>
-      </>
+            <CloseIconReactSvg src={closeIcon} />
+          </AddIngedientSelect>
+        </>
 
-      <SubTitle>Recipe Preparation</SubTitle>
-      <label htmlFor="addDrinkFormTextarea">Enter the recipe</label>
-      <AddFormTextarea id="addDrinkFormTextarea"></AddFormTextarea>
-      <WhiteLinkBtn type="submit" title="Add" />
-    </form>
+        <SubTitle>Recipe Preparation</SubTitle>
+        <label htmlFor="addDrinkFormTextarea">Enter the recipe</label>
+        <AddFormTextarea id="addDrinkFormTextarea"></AddFormTextarea>
+        <WhiteLinkBtn type="submit" title="Add" />
+      </form>
+    </>
   );
 }
