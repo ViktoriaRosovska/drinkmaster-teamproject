7ecf05d84197756119765ff38826942f89a095ff
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-11-02 23:09:07 +0200

    fix

--- src/views/FavouriteDrinksPage/FavouriteDrinksPage.jsx
+++ src/views/FavouriteDrinksPage/FavouriteDrinksPage.jsx
@@ -28,6 +28,7 @@ export default function FavoriteDrinksPage() {
 
   useEffect(() => {
     navigate(`?page=${currentPage}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
