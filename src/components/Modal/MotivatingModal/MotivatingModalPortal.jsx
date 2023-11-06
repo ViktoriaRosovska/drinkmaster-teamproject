27d0ce8bbb9c244317c71675c9aeb33549a8cf7c
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-11-06 14:47:54 +0200

    test version of motivating modal

--- src/components/Modal/MotivatingModal/MotivatingModalPortal.jsx
+++ src/components/Modal/MotivatingModal/MotivatingModalPortal.jsx
@@ -1,37 +1,46 @@
 import { createPortal } from 'react-dom';
 import MotivatingModal from './MotivatingModal';
-import { motivationScenarios } from './motivatingScenarios';
-import { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { authOperations } from 'redux/auth/authOperations';
+import { motivatingScenarios } from './motivatingScenarios';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/authSelectors';
+import { useDrink } from 'hooks/useDrink';
 
 const MotivatingModalPortal = () => {
-    const modalRoot = document.querySelector('#modal-root');
-    
-    // const dispatch = useDispatch();
+  const modalRoot = document.querySelector('#modal-root');
+  const userAuth = useSelector(authSelectors.selectUserAuth);
+  console.log('userAuth:', userAuth);
+  const { favoriteDrinks } = useDrink();
+  console.log(favoriteDrinks.length);
 
-  const [isOpen, setIsOpen] = useState(true);
-//   const [motivatingScenario, setMotivatingScenario] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [motivatingScenario, setMotivatingScenario] = useState(null);
 
-  const motivatingScenario = motivationScenarios.scenario3;
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await dispatch(authOperations.currentUser());
-    //             console.log(response);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     };
-    //     fetchData();
-    // }, []);
-         
-
-
+  //   const motivatingScenario = motivationScenarios.scenario3;
   const closeModal = () => {
     setIsOpen(false);
+
+    localStorage.setItem('motivatingModalClosed', 'true');
   };
+
+  useEffect(() => {
+    const isModalClosed =
+      localStorage.getItem('motivatingModalClosed') === 'true';
+
+    if (!isModalClosed) {
+      if (userAuth === 10) {
+        setMotivatingScenario(motivatingScenarios.scenario1);
+      } else if (favoriteDrinks.length === 1) {
+        setMotivatingScenario(motivatingScenarios.scenario2);
+      } else if (favoriteDrinks.length === 10) {
+        setMotivatingScenario(motivatingScenarios.scenario3);
+      }
+
+      if (motivatingScenario) {
+        setIsOpen(true);
+      }
+    }
+  }, [userAuth, favoriteDrinks, motivatingScenario]);
 
   return isOpen
     ? createPortal(
