06144c53dff3fe55433b798d201d949104487567
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-11-07 11:10:04 +0200

    save-name-and-avatar

--- src/components/Modal/UserInfoModal/UserInfoModal.jsx
+++ src/components/Modal/UserInfoModal/UserInfoModal.jsx
@@ -18,29 +18,71 @@ import AddPhoto from '../../../assets/images/userInfoModal/addPhoto.svg';
 import UserAvatar from '../../../assets/images/userInfoModal/userAvatar.svg';
 import { useState } from 'react';
 import IconCloseModal from '../IconCloseModal/IconCloseModal';
+import { updateUser } from 'redux/auth/authOperations';
+import { useDispatch, useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/authSelectors';
 // import useAuth from 'hooks/useAuth';
 // import { useDispatch } from 'react-redux';
 
 export default function UserInfoModal({ onBackdropClose }) {
-  // const {
-  //   user: { avatarURL, name },
-  // } = useAuth();
+  const dispatch = useDispatch();
+  const { avatar, name } = useSelector(authSelectors.selectUser);
+  const [avatarURL, setAvatarURL] = useState("");
+  const [userData, setUserData] = useState({
+    avatar,
+    name,
+  });
 
-  // const [username, setUsername] = useState(name);
-  // const [avatar, setAvatar] = useState(null);
-  const [file, setFile] = useState();
+  function handleAvatarChange(e) {
+    // const selectedFile = (e.target.files[0]);
 
-  // const dispatch = useDispatch();
+    // // if (selectedFile) {
 
-  function handleChange(e) {
-    setFile(URL.createObjectURL(e.target.files[0]));
+    // //   const fileReader = new FileReader();
+    // //   fileReader.onload = function (e) {
+    // //     const fileContent = e.target.result;
+    // //     console.log('File content:', fileContent);
+    // //   };
+    // //   fileReader.readAsText(selectedFile);
+    // //   setUserData({...userData, avatar: selectedFile});
+    // // }
+
+    const avatarFile = e.target.files[0];
+    
+      setUserData({ ...userData, avatar: avatarFile });
+   
+      const avatarUrl = URL.createObjectURL(e.target.files[0]);
+      setAvatarURL(avatarUrl);
+    
   }
 
-  // useEffect(() => {
-  //   if (avatarURL) {
-  //     setAvatar(avatarURL);
-  //   }
-  // }, [avatarURL]);
+  const handleNameChange = e => {
+    // Обробка зміни імені користувача
+    const name = e.target.value;
+    setUserData({ ...userData, name });
+  };
+
+  const handleSubmitUpdateUser = async () => {
+    const formData = new FormData();
+    if (userData.avatar) {
+      formData.append('avatar', userData.avatar);
+    }
+    formData.append('name', userData.name);
+
+    try {
+      // setIsLoading(true);
+      await dispatch(updateUser(formData));
+      // setIsLoading(false);
+
+      formData.reset();
+      onBackdropClose();
+    } catch (error) {
+      console.log('Помилка сабміту при оновлені профилю', error.message);
+    }
+    formData.reset();
+    // Після успішної відповіді закрити модальне вікно
+    onBackdropClose();
+  };
 
   return (
     <ModalInfoContainer>
@@ -48,16 +90,29 @@ export default function UserInfoModal({ onBackdropClose }) {
       <Ellipse224 />
       <ImageWrapper>
         <LabelAddFile>
-          <InputAddFile type="file" onChange={handleChange} />
+          <InputAddFile
+            type="file"
+            accept="image/*"
+            onChange={handleAvatarChange}
+          />
           <ReactSVGIcon src={AddPhoto} />
         </LabelAddFile>
-        <AvatarImg src={file ? file : UserAvatar} alt="User avatar" />
+        <AvatarImg
+          src={avatarURL ? avatarURL : userData.avatar || UserAvatar}
+          alt="User avatar"
+        />
       </ImageWrapper>
-      <StyledForm>
+      <StyledForm onSubmit={handleSubmitUpdateUser}>
         <StyledLabel>
-          <StyledInput type="text" />
+          <StyledInput
+            type="text"
+            value={userData.name}
+            onChange={handleNameChange}
+          />
           <IconEdit />
-          <SaveUserInfoBtn type="submit">Save changes</SaveUserInfoBtn>
+          <SaveUserInfoBtn type="submit" onClick={handleSubmitUpdateUser}>
+            Save changes
+          </SaveUserInfoBtn>
         </StyledLabel>
       </StyledForm>
       <CloseModalBtn type="button" onClick={() => onBackdropClose()}>
