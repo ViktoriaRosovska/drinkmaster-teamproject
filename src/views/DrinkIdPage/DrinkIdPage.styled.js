8c0e2edce7b6a2e9c0698d6ab02daf1fa57e32d6
Dmytro Medvediev <dsisitkin@gmail.com>
2023-11-07 20:39:38 +0100

    Fix styles

--- src/views/DrinkIdPage/DrinkIdPage.styled.js
+++ src/views/DrinkIdPage/DrinkIdPage.styled.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
+color: ${props => props.theme.color};
   font-size: 32px;
   margin-bottom: 8px;
   font-weight: 600;
+  color: ${props => props.theme.color};
 
   @media screen and (min-width: 768px) {
     font-size: 56px;
