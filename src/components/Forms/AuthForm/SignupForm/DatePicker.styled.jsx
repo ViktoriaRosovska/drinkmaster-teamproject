a6edbe97ae72b5a978352aae37df029a7957743a
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-11-08 13:12:59 +0200

    Update DatePicker.styled.jsx

--- src/components/Forms/AuthForm/SignupForm/DatePicker.styled.jsx
+++ src/components/Forms/AuthForm/SignupForm/DatePicker.styled.jsx
@@ -1,71 +1,73 @@
 import { styled } from '@mui/material/styles';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { CalendarSvgIcon } from './CalendarSvgIcon';
 
 import { useFormikContext } from 'formik';
 
 const transition = '300ms ease-in';
 
-const StyledDesktopDatePicker = styled(DatePicker)(({ err, curValue }) => {
-  return {
-    '& .MuiInputBase-root': {
-      borderRadius: '200px',
-      border: '1px solid var(--white-twenty-color)',
-      outline: 'none',
-      fontSize: '14px',
-      lineHeight: '1.28',
-      color: 'var(--white-fifty-color)',
-      backgroundColor: 'transparent',
-      transition: `border 300ms ease-in`,
-      borderColor:
-        err && curValue
-          ? 'red'
-          : curValue
-          ? 'green'
-          : 'var(--white-twenty-color)',
-    },
+const StyledDesktopDatePicker = styled(DesktopDatePicker)(
+  ({ err, curValue }) => {
+    return {
+      '& .MuiInputBase-root': {
+        borderRadius: '200px',
+        border: '1px solid var(--white-twenty-color)',
+        outline: 'none',
+        fontSize: '14px',
+        lineHeight: '1.28',
+        color: 'var(--white-fifty-color)',
+        backgroundColor: 'transparent',
+        transition: `border 300ms ease-in`,
+        borderColor:
+          err && curValue
+            ? 'var(--red-error-fifty-color)'
+            : curValue
+            ? 'var(--green-success-fifty-color)'
+            : 'var(--white-twenty-color)',
+      },
 
-    '& .MuiInputBase-root:hover': {
-      border: '1px solid var(--white-fifty-color)',
-    },
+      '& .MuiInputBase-root:hover': {
+        border: '1px solid var(--white-fifty-color)',
+      },
 
-    '& .MuiTextField-root': {
-      padding: '18px 24px',
-    },
+      '& .MuiTextField-root': {
+        padding: '18px 24px',
+      },
 
-    '& .MuiOutlinedInput-root': {
-      fontSize: '14px',
-      lineHeight: '1.28',
-      color: 'var(--white-fifty-color)',
-      backgroundColor: 'transparent',
-      paddingRight: '18px',
-    },
+      '& .MuiOutlinedInput-root': {
+        fontSize: '14px',
+        lineHeight: '1.28',
+        color: 'var(--white-fifty-color)',
+        backgroundColor: 'transparent',
+        paddingRight: '18px',
+      },
 
-    '& .MuiInputBase-input': {
-      padding: '18px 24px',
-    },
+      '& .MuiInputBase-input': {
+        padding: '18px 24px',
+      },
 
-    '& .MuiIconButton-root': {
-      marginRight: '0px',
-    },
+      '& .MuiIconButton-root': {
+        marginRight: '0px',
+      },
 
-    '& .Mui-error': {
-      transition: `border ${transition} color ${transition}`,
-      border: '1px solid var(--red-error-fifty-color)',
-    },
+      '& .Mui-error': {
+        transition: `border ${transition} color ${transition}`,
+        border: '1px solid var(--red-error-fifty-color)',
+      },
 
-    '& .MuiOutlinedInput-notchedOutline': {
-      border: 'none',
-    },
+      '& .MuiOutlinedInput-notchedOutline': {
+        border: 'none',
+      },
 
-    ///////////////// Calendar ////////////////////
+      ///////////////// Calendar ////////////////////
 
-    // '& .MuiDateCalendar-root': {
-    //   backgroundColor: 'var(--dark-blue-color)',
-    //   borderRadius: '200',
-    // },
-  };
-});
+      // '& .MuiDateCalendar-root': {
+      //   backgroundColor: 'var(--dark-blue-color)',
+      //   borderRadius: '200',
+      // },
+    };
+  }
+);
 
 export default function BirthDate() {
   const { setFieldValue, errors, values } = useFormikContext();
