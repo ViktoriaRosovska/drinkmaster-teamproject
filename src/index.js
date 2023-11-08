fd753481b1df35c59caec2e784e5fb6f3c4a5783
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-11-08 15:43:13 +0200

    styles for calendar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import './assets/fonts/stylesheet.css';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from '../src/redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import Loader from 'components/Loader';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
